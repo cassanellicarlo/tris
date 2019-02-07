82e987751f8324485e62e1be17b7ae725172d3fa
cassanellicarlo <cassatek@gmail.com>
2019-02-07 14:42:11 +0100

    code improvements

--- src/app/components/gioco/gioco.component.ts
+++ src/app/components/gioco/gioco.component.ts
@@ -46,33 +46,29 @@ export class GiocoComponent implements OnInit {
 
   }
 
-  // Ad ogni mossa di un giocatore
+  // Mossa di un giocatore
   makeMove (riga:number,colonna:number) {
 
     if(!this.finito){ // Se il gioco non è finito
 
       if(this.griglia[riga][colonna].libero){ // Se il quadrato è libero
+        
         this.mosse++;
-
         this.griglia[riga][colonna]=new Quadrato(this.giocatoreAttuale,false);
-  
-        this.checkWinVertical();
-        this.checkWinHorizontal();
-        this.checkWinDiagonal();
+
+        this.checkRow(riga); 
+        this.checkColumn(colonna);
+        this.checkDiagonals();
         this.cambiaGiocatore(); 
 
-        console.log("MOSSA: "+this.mosse);
         if(this.mosse == (this.dimensioneGriglia*this.dimensioneGriglia) && !this.finito){
           this.messaggio="Gioco finito! E' finita Pari.";
           this.messaggio2=undefined;
-        }
-  
-        
+        }       
         
       }
   
       else{ // Se il quadrato è occupato
-        console.log("Quadrato occupato!");
         this.messaggio="Quadrato occupato!";
       }
     }
@@ -80,73 +76,56 @@ export class GiocoComponent implements OnInit {
     else {
       this.messaggio="Il gioco è finito! Clicca su Start Game per ricominciare";
       this.messaggio2=undefined;
-    }
-
-
-    
-  }
-
-  // Controlla la vincita di un giocatore (in orizzontale)
-  checkWinHorizontal (){
-
-    this.griglia.forEach( (riga) => {
-        
-      const rigaUguale = riga.reduce ( (a,b) => a.valore===b.valore ? a : false);
-
-      if(rigaUguale.valore!="" && rigaUguale!=false){
-        console.log("Vincita orizzontale Giocatore:" +this.giocatoreAttuale);
-        this.vincita();
-      }
-
-    });
+    }    
 
   }
 
-  // Controlla la vincita di un giocatore (in verticale)
-  checkWinVertical (){
+  // Controlla la vincita di un giocatore su una riga
+  checkRow (riga:number){
+    const rigaUguale = this.checkAllEquals(this.griglia[riga]);
+    this.checkWin(rigaUguale);
+  }
 
-    for(let i=0;i<this.griglia.length;i++){
-      const colonna=[];
-      for(let j=0;j<this.griglia[i].length;j++){
-        colonna.push(this.griglia[j][i]);
-      }
-
-      const colonnaUguale = colonna.reduce ( (a,b) => a.valore===b.valore ? a : false);
-
-      if(colonnaUguale.valore!="" && colonnaUguale!=false){
-        console.log("Vincita verticale Giocatore:" +this.giocatoreAttuale);
-        this.vincita();
-      }
-
+  // Controlla la vincita di un giocatore su una colonna
+  checkColumn (colonna:number){
+    const col=[];
+    for(let i=0;i<this.dimensioneGriglia;i++){
+      col.push(this.griglia[i][colonna]);
     }
-
+    const colonnaUguale = this.checkAllEquals(col);
+    this.checkWin(colonnaUguale);
   }
 
   // Controlla la vincita di un giocatore nelle due diagonali
-  checkWinDiagonal (){
+  checkDiagonals (){
     
     const diagonale1 = [];
     const diagonale2 = [];
 
-    for(let i=0;i<this.griglia.length;i++){
-      diagonale1.push(this.griglia[i][i].valore);
-      diagonale2.push(this.griglia[i][this.griglia.length-i]);
+    for(let i=0;i<this.dimensioneGriglia;i++){
+      diagonale1.push(this.griglia[i][i]);
+      diagonale2.push(this.griglia[i][this.dimensioneGriglia-1-i]);
     }
 
-    const diagonale1Uguale = diagonale1.reduce ( (a,b) => a===b ? a : false);
+    const diagonale1Uguale = this.checkAllEquals(diagonale1);
+    const diagonale2Uguale = this.checkAllEquals(diagonale2);
 
-    if(diagonale1Uguale.valore!="" && diagonale1Uguale!=false){
-      console.log("Vincita diagonale 1 Giocatore:" +this.giocatoreAttuale);
+    this.checkWin(diagonale1Uguale);
+    this.checkWin(diagonale2Uguale);
+
+  }
+
+  // Controlla il valore passato alla fine della reduce
+  // Se è diverso da false l'array contiene valori tutti uguali e il giocatore ha vinto
+  checkWin (result){
+    if(result!=false){
       this.vincita();
     }
+  }
 
-    const diagonale2Uguale = diagonale2.reduce ( (a,b) => a===b ? a : false);
-
-    if(diagonale2Uguale.valore!="" && diagonale2Uguale!=false){
-      console.log("Vincita diagonale 2 Giocatore:" +this.giocatoreAttuale);
-      this.vincita();
-    }
-
+  // Controlla se gli elementi dell'array passato sono tutti uguali
+  checkAllEquals (array){
+    return array.reduce ( (a,b) => a.valore===b.valore && a.valore!="" ? a : false);
   }
 
   // Cambio il giocatore attuale ad ogni mossa
@@ -161,6 +140,7 @@ export class GiocoComponent implements OnInit {
     }
   }
 
+  // Vincita di un giocatore e fine del gioco
   vincita (){
     this.finito=true;
     this.messaggio="Ha vinto il Giocatore: "+this.giocatoreAttuale;
